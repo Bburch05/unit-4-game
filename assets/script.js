19fec1d08684d64989707728eca313e7b75c1ba7
Brian Burch <Bburch05@gmail.com>
2018-06-07 21:27:59 -0400

    changes

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+var luke = {
+    atkBase : 10,
+    atk : 10,
+    hp : 100,
+    cAtk : 15,
+}
+
+var obi = {
+    atkBase : 10,
+    atk : 10,
+    hp : 100,
+    cAtk : 15,
+}
+
+var jar = {
+    atkBase : 10,
+    atk : 10,
+    hp : 100,
+    cAtk : 15,
+}
+
+var darthmaul = {
+    atkBase : 10,
+    atk : 10,
+    hp : 100,
+    cAtk : 15,
+}
+
+var luuke = {
+    atkBase : 10,
+    atk : 10,
+    hp : 100,
+    cAtk : 15,
+}
+
+var chars = [luke,obi,jar,luuke]
+var heroChoice;
+var enemyChoice;
+var heroChoice = chars[this.value]
